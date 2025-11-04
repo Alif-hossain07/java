89841c19823437c4029015d929714f7c0d69ff4a
Alif hossain <alifhossain72003@gmail.com>
2025-11-05 01:34:20 +0600

    Initial Commit

---
+++ Lab Task/Task2.java
@@ -0,0 +1,56 @@
+
+public class Task2 {
+    public static void main(String[] args) {
+        String firstName = "Alif";
+        String lastName = "Hossain";
+
+        String fullName = firstName + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        String productName = "Mobile";
+        int productID = 101;
+
+        System.out.println("product code :"+productID)
+    }
+}
+
+public class Task2 {
+    public static void main (String[] args){
+     String email ="alifhossain989@gmail.com";
+     String username = String.substring(0,email.indexof(0));
+    system.out.println("Username:"+username);
+    }
+}
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        String message = "Hello there";
+        int totalChars = message.length();
+
+        System.out.println("Total characters: " + totalChars);
+    }
+}
+
+public class Task2 {
+    public static void main(String[] args) {
+        String sentence = "Java is case sensitive";
+
+        System.out.println("Uppercase: " + sentence.toUpperCase());
+        System.out.println("Lowercase: " + sentence.toLowerCase());
+    }
+}
+
+public class Task2{
+    public static void main(String[] args){
+        String S="I am Batman";
+        String C =s.replace ("B","T");
+        System.ot.println(C);
+    }
+}

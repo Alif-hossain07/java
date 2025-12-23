8d7c79bc863880ac6cd0e5b1603f468da04d99f1
Alif hossain <alifhossain72003@gmail.com>
2025-12-24 05:58:48 +0600

    Implement AbstractEmployee and FullTimeEmployee classes

---
+++ Lab Task/AbstractEmployee.java
@@ -0,0 +1,40 @@
+public class AbstractEmployee {
+abstract class Employee {
+
+    String name;
+
+    Employee(String name) {
+        this.name = name;
+    }
+
+    abstract double calculateSalary();
+
+    void showDetails() {
+        System.out.println("Employee Name: " + name);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+
+    double monthlySalary;
+
+    FullTimeEmployee(String name, double monthlySalary) {
+        super(name);
+        this.monthlySalary = monthlySalary;
+    }
+
+    double calculateSalary() {
+        return monthlySalary;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        FullTimeEmployee emp = new FullTimeEmployee("Rahim", 30000);
+
+        emp.showDetails();
+        System.out.println("Salary: " + emp.calculateSalary());
+    }
+}
+}
